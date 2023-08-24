3a7175430f4553c17517dae85c2c57723034872f
Julien <julien@goliaths.io>
2023-08-24 03:05:11 +0200

    seperate tests

--- test/Hate.test.ts
+++ test/Hate.test.ts
@@ -9,69 +9,86 @@ import { ethers } from "hardhat";
 import { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers";
 import { ContractRunner } from "ethers";
 
-/*
-
-const amountValue = ethers.parseUnits(amount.toString(), decimals)
-const amountTxt = ethers.formatUnits(amountValue, decimals)
-
-*/
+const toWei = (val: number) => ethers.parseUnits(val.toString(), 18)
+const displayEther = (val: number | bigint) => ethers.formatUnits(val, 18)
 
 let contract: HateMe
+
+const bucketSlug = ethers.toUtf8Bytes("julius")
+const amount = ethers.parseUnits("5", 18)
 
 const getBalance = async (address: string) =>
   await ethers.provider.getBalance(address)
 
 const getFormattedBalance = async (address: string) =>
-  ethers.formatUnits(await getBalance(address))
-
-let steps: any = []
+  displayEther(await getBalance(address))
 
 const log = async (action?: string) => {
   const [owner, user2] = await ethers.getSigners()
   const state = {
     User1: await getFormattedBalance(owner.address),
-    User1_Claimable: ethers.formatUnits(await contract.gains(owner.address)),
+    User1_Claimable: displayEther((await contract.buckets(bucketSlug)).claimable),
     _: '----------------',
     User2: await getFormattedBalance(user2.address),
-    User2_Claimable: ethers.formatUnits(await contract.gains(user2.address)),
   }
   console.log(action)
   console.table(state)
 }
 
 describe("HateMe", function () {
-  it("hate someone", async function () {
-    const [owner, user2] = await ethers.getSigners()
-
+  it("create bucket", async function () {
+    const [owner] = await ethers.getSigners()
     contract = await deployContract()
-    const contractAddr = await contract.getAddress()
 
-    const amount = ethers.parseUnits("5", 18)
-    const bucketSlug = ethers.toUtf8Bytes("julius")
-   
     await expect(
       contract.connect(owner).createBucket(bucketSlug)
     ).to.emit(contract, "BucketCreated")
+  })
+
+  it("bucket already exists", async function () {
+    const [owner] = await ethers.getSigners()
 
     await expect(
       contract.connect(owner).createBucket(bucketSlug)
     ).to.revertedWith("Bucket already exists")
+  })
+
+  it("hate someone", async function () {
+    const [_, user2] = await ethers.getSigners()
+    const contractAddr = await contract.getAddress()   
 
     await expect(
       contract.connect(user2).hateMe(bucketSlug, { value: amount })
     ).to.changeEtherBalance(contractAddr, amount)
+  })
+
+  it("love someone", async function () {
+    const [_, user2] = await ethers.getSigners()
+    const contractAddr = await contract.getAddress() 
 
     await expect(
       contract.connect(user2).kiddingILoveYou(bucketSlug, { value: amount })
     ).to.changeEtherBalance(contractAddr, amount)
-    
+  })
+
+  it("non-owner can't claim", async function () {
+    const [_, user2] = await ethers.getSigners()
+
     await expect(
       contract.connect(user2).claim(bucketSlug)
     ).to.revertedWith("You're not the owner")
+  })
+  
+  it("owner can claim", async function () {
+    const [owner] = await ethers.getSigners()
     
     await expect(
       contract.connect(owner).claim(bucketSlug)
     ).to.changeEtherBalance(owner, amount + amount)
+  })
+
+  it("can't claim twice", async function () {
+    const [owner] = await ethers.getSigners()
     
     await expect(
       contract.connect(owner).claim(bucketSlug)
@@ -79,7 +96,6 @@ describe("HateMe", function () {
   })
 
   it("can't create bad slugs", async function () {
-
     const tooShort = ethers.toUtf8Bytes("ju")
     const tooLong = ethers.toUtf8Bytes(Array(50).fill(0).toString())
     
